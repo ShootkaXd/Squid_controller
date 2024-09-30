85a1a60cf7c0101948ab92b67763b41560eddf6b
Lermontov <Lermontov@DESKTOP-7BL9S2K>
2024-09-30 13:59:57 +0300

    sdasda

--- app.py
+++ app.py
@@ -93,8 +93,6 @@ def handle_connect():
 def monitor():
     return render_template('monitoring_realtime.html')
 
-def dasdasdas():
-
 
 ####################### Системный монитор ##########################
 
