05d62c7dc0fcf09147729bf81ec5ea5d2e99832b
Lermontov <Lermontov@DESKTOP-7BL9S2K>
2024-10-04 09:53:53 +0300

    optimization

--- app.py
+++ app.py
@@ -219,4 +219,4 @@ if __name__ == '__main__':
     scan_thread = threading.Thread(target=run_periodic_scan)
     scan_thread.daemon = True
     scan_thread.start()
-    app.run(host=settings.host, port=5000, debug=True)
+    app.run(host=settings.host, port=5000)
