de2e6c6a89288ced105b4552d4f34c526d8fc72e
Lermontov <Lermontov@DESKTOP-7BL9S2K>
2024-10-14 08:25:58 +0300

    fix bug

--- database.py
+++ database.py
@@ -28,7 +28,7 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         status TEXT DEFAULT "offline",
-        ip_address TEXT,
+        ip_address TEXT UNIQUE,
         mac_address TEXT UNIQUE,
         hostname TEXT,
         last_seen TEXT,
@@ -48,8 +48,8 @@ CREATE TABLE IF NOT EXISTS blocked_sites(
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS new_users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        ip_address TEXT,
-        mac_address TEXT,
+        ip_address TEXT UNIQUE,
+        mac_address TEXT UNIQUE,
         username TEXT,
         department TEXT,
         number_cabinet TEXT
